5e2926ecfee14d13a2995fbb4b04a7aa8e0f23f0
sing-u8 <singyu13@gmail.com>
2021-12-01 17:55:22 +0900

    Preventing Unintended HTTP Requests using the HTTP Testing Controller

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -32,11 +32,8 @@ describe('CoursesService', () => {
     coursesService.findAllCourses().subscribe(courses => {
 
       expect(courses).toBeTruthy('No courses returned')
-
       expect(courses.length).toBe(12, "incorrect number of courses")
-
       const course = courses.find(course => course.id == 12)
-
       expect(course.titles.description).toBe("Angular Testing Course")
 
     })
@@ -53,7 +50,34 @@ describe('CoursesService', () => {
     // 옵저버블로 받은 데이터는 구독 함수에서 검사합니다.
     req.flush({payload: Object.values(COURSES)})
 
+  })
 
+
+  it('should find a course by id', () => {
+
+    // 바로 실제 서버에 요청되지 않음
+    // req.flush가 실행되면 구독이 시작됨
+    coursesService.findCourseById(12).subscribe(courses => {
+      expect(courses).toBeTruthy('No courses returned')
+      expect(courses.id).toBe(12, "incorrect id of course")
+    })
+
+    // /api/courses in findAllCourses method of CoursesService
+    // `expectOne()`은 HTTP 요청의 URL과 매칭됩니다.
+    // 이 주소로 HTTP 요청이 발생하지 않거나 여러번 요청되면 에러를 반환합니다.
+    const req = httpTestingController.expectOne('/api/courses/12')
+
+    // HTTP 요청 방식이 GET인지 검사합니다.
+    expect(req.request.method).toEqual('GET')
+
+    // 목업 데이터로 응답을 보내면 옵저버블이 종료됩니다.
+    // 옵저버블로 받은 데이터는 구독 함수에서 검사합니다.
+    req.flush(COURSES[12])
+
+
+  })
+
+  afterEach(() => {
     httpTestingController.verify();
   })
 
