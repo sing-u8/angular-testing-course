4c0ec2ca4e4c759058cbe81ebc97f8909dc51632
sing-u8 <singyu13@gmail.com>
2021-12-01 21:30:12 +0900

    Angular HTTP Testing - Checking HTTP Request Parameters

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -3,8 +3,9 @@ import { TestBed } from '@angular/core/testing'
 import {HttpTestingController , HttpClientTestingModule } from '@angular/common/http/testing'
 import {CoursesService} from './courses.service'
 
-import { COURSES } from '../../../../server/db-data'
+import { COURSES, findLessonsForCourse } from '../../../../server/db-data'
 import { Course } from '../model/course'
+import { HttpErrorResponse } from '@angular/common/http'
 
 describe('CoursesService', () => {
 
@@ -81,11 +82,8 @@ describe('CoursesService', () => {
   it('should save the course data', () => {
 
     const changes : Partial<Course> =  {titles: {description: 'Testing Course'}}
-
     coursesService.saveCourse(12, changes).subscribe(course => {
-
       expect(course.id).toBe(12)
-
     })
 
     const req = httpTestingController.expectOne('/api/courses/12')
@@ -95,6 +93,48 @@ describe('CoursesService', () => {
     req.flush({...COURSES[12], ...changes})
 
   })
+
+  it('should give an error if save course fails', () => {
+
+    const changes : Partial<Course> =  {titles: {description: 'Testing Course'}}
+    coursesService.saveCourse(12, changes).subscribe(course => {
+      fail('the save course operation should have failed')
+    },
+    (err: HttpErrorResponse) => {
+      expect(err.status).toBe(500)
+
+    })
+
+    const req = httpTestingController.expectOne('/api/courses/12')
+    expect(req.request.method).toEqual("PUT")
+    req.flush('Save course failed', {status: 500, statusText: 'Internal Server Error'})
+
+  })
+
+  it('should find a list of lessons', () => {
+
+    coursesService.findLessons(12).subscribe(lessons => {
+      expect(lessons).toBeTruthy()
+      expect(lessons.length).toBe(3)
+    })
+
+    const req = httpTestingController.expectOne(req => req.url == '/api/lessons')
+
+    expect(req.request.method).toEqual('GET')
+
+    expect(req.request.params.get("courseId")).toEqual("12")
+    expect(req.request.params.get("filter")).toEqual("")
+    expect(req.request.params.get("sortOrder")).toEqual("asc")
+    expect(req.request.params.get("pageNumber")).toEqual("0")
+    expect(req.request.params.get("pageSize")).toEqual("3")
+
+    req.flush({
+      payload: findLessonsForCourse(12).slice(0, 3)
+    })
+
+  })
+
+
 
   afterEach(() => {
     httpTestingController.verify();
